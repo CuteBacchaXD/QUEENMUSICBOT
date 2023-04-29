10a6eed58efe822f2f0fb08a956529998494b1a4
CUTE BACCHA <132035388+CuteBacchaXD@users.noreply.github.com>
2023-04-30 02:50:05 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -4,30 +4,30 @@ import aiohttp
 from os import getenv
 from dotenv import load_dotenv
 
-# 𝐈𝐧𝐭𝐞𝐫𝐧𝐚𝐥 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 (@𝐁𝐢𝐤𝐚𝐬𝐡𝐇𝐚𝐥𝐝𝐞𝐫 @𝐀𝐝𝐢𝐭𝐲𝐚𝐇𝐚𝐥𝐝𝐞𝐫)
+# 𝐈𝐧𝐭𝐞𝐫𝐧𝐚𝐥 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 (@I_LOVE_YOU_MY_HEARTBEET)
 load_dotenv()
 que = {}
 admins = {}
 aiohttpsession = aiohttp.ClientSession()
 
-# 𝐑𝐞𝐪𝐮𝐢𝐫𝐞𝐝 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 //𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 @𝐁𝐢𝐤𝐚𝐬𝐡𝐇𝐚𝐥𝐝𝐞𝐫 @𝐀𝐝𝐢𝐭𝐲𝐚𝐇𝐚𝐥𝐝𝐞𝐫
+# 𝐑𝐞𝐪𝐮𝐢𝐫𝐞𝐝 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 //𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 @I_LOVE_YOU_MY_HEARTBEET
 API_HASH = getenv("API_HASH", "XXXXX")
 API_ID = int(getenv("API_ID", "XXXXX"))
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "XXXXX")
 ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME", "XXXXX")
-BOT_IMAGE = getenv("BOT_IMAGE", "https://te.legra.ph/file/99d0261f0aa5512ad6753.jpg")
+BOT_IMAGE = getenv("BOT_IMAGE", "https://graph.org/file/a8504a33f2a20b63f8adf.jpg")
 BOT_NAME = getenv("BOT_NAME", "XXXXX")
 BOT_TOKEN = getenv("BOT_TOKEN", "12345:XXXXX")
 BOT_USERNAME = getenv("BOT_USERNAME", "XXXXX")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
-OWNER_NAME = getenv("OWNER_NAME", "𝐁𝐢𝐤𝐚𝐬𝐡 𝐇𝐚𝐥𝐝𝐞𝐫")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "BikashHalder")
-SOURCE_CODE = getenv("SOURCE_CODE", "https://github.com/BikashhalderNew/BGTMUSICBOT2")
+OWNER_NAME = getenv("OWNER_NAME", "𝑸𝑼𝑬𝑬𝑵")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "I_LOVE_YOU_MY_HEARTBEET")
+SOURCE_CODE = getenv("SOURCE_CODE", "https://github.com/CuteBacchaXD/QUEENMUSICBOT")
 STRING_SESSION = getenv("STRING_SESSION", "session")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1439222689").split()))
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/bgt_chat")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "https://t.me/BikashGadgetsTech")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1952625698").split()))
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ISHQ00_I")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "https://t.me/XD_CUTETY")
 
 # 𝐃𝐨 𝐍𝐨𝐭 𝐂𝐡𝐚𝐧𝐠𝐞 𝐓𝐡𝐢𝐬 𝐋𝐢𝐧𝐞𝐬 // 𝐈𝐠𝐧𝐨𝐫𝐞 𝐓𝐡𝐢𝐬 (𝐁𝐢𝐤𝐚𝐬𝐡 𝐇𝐚𝐥𝐝𝐞𝐫) 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-PROFILE_CHANNEL = getenv("PROFILE_CHANNEL", "https://t.me/BikashDP")
+PROFILE_CHANNEL = getenv("PROFILE_CHANNEL", "https://t.me/S3D_LIF3")
